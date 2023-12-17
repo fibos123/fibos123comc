217056e96eff74e00ac7d545e7a09fd0e75dd154
Mingfun Wong <mingfun.wong.chn@gmail.com>
2023-12-17 12:11:29 +0900

    use eosjs

--- src/get.ts
+++ src/get.ts
@@ -1,14 +1,16 @@
 import fs from "fs";
-import { fibos } from "./lib/fibos";
+import { JsonRpc } from "./libs/fibos";
 
 async function main() {
+  const endpoint = process.env.httpEndpoint as string;
+  const rpc = new JsonRpc(endpoint, { fetch });
   const filePath = "./sites.json";
 
   if (fs.existsSync(filePath)) {
     throw new Error("File already exists!");
   }
 
-  const jsons = await fibos.getTableRows({
+  const jsons = await rpc.get_table_rows({
     json: true,
     code: "fibos123comc",
     scope: "fibos123comc",
@@ -18,10 +20,14 @@ async function main() {
     upper_bound: "",
     limit: 1,
   });
+
   const sitesText = jsons.rows[0].text;
   const sitesJson = JSON.parse(sitesText);
   const sitesAns = JSON.stringify(sitesJson, null, 2);
+
   fs.writeFileSync(filePath, sitesAns, "utf8");
+
+  console.log(`Created ${filePath}`);
 }
 
 main();
--- src/set.ts
+++ src/set.ts
@@ -1,25 +1,48 @@
 import fs from "fs";
-import { fibos } from "./lib/fibos";
+import { Api, JsonRpc } from "./libs/fibos";
+import { JsSignatureProvider } from "eosjs/dist/eosjs-jssig";
 
 async function main() {
   const accountName = process.env.accountName as string;
+  const endpoint = process.env.httpEndpoint as string;
+  const privateKey = process.env.privateKey as string;
+
+  const signatureProvider = new JsSignatureProvider([privateKey]);
+  const rpc = new JsonRpc(endpoint, { fetch });
+  const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
+
   const filePath = "./sites.json";
 
   if (!fs.existsSync(filePath)) {
     throw new Error("File does not exist!");
   }
   const sitesText = fs.readFileSync(filePath, "utf8");
-  
-  const contract = await fibos.contract("fibos123comc");
-  const result = await contract.update(
-    {
-      id: "sites",
-      text: sitesText,
-    },
-    {
-      authorization: accountName,
-    }
-  );
+
+  const transaction = {
+    actions: [
+      {
+        account: "fibos123comc",
+        name: "update",
+        authorization: [
+          {
+            actor: accountName,
+            permission: "active",
+          },
+        ],
+        data: {
+          id: "sites",
+          text: sitesText,
+        },
+      },
+    ],
+  };
+  const transactConfig = {
+    blocksBehind: 3,
+    expireSeconds: 30,
+  };
+
+  const result = await api.transact(transaction, transactConfig);
+
   console.log(result);
 }
 
